964daeeda4a2f1e44b8d10e9d613af7713941a8c
SouthpawCurveConcepts <jagoblacksky@gmail.com>
2022-02-23 12:00:50 -0600

    changes

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMover : MonoBehaviour
+public class Driver : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
