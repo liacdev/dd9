8e07b48073a5a785877518228299091699af5c1a
SouthpawCurveConcepts <jagoblacksky@gmail.com>
2022-02-24 12:06:53 -0600

    OnTriggerEnter

--- Assets/Scripts/Collision.cs
+++ Assets/Scripts/Collision.cs
@@ -7,4 +7,7 @@ public class Collision : MonoBehaviour
     void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("Ouch!");
     }
+     void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("Zoom!");
+    }
 }
