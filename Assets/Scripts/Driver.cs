6c1a0e04045699ac2b0474f285ce649c55e7c0e5
SouthpawCurveConcepts <jagoblacksky@gmail.com>
2022-02-23 19:26:29 -0600

    turn and speed

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    [SerializeField] float turnSpeed = 1f;
+    [SerializeField] float moveSpeed = 0.01f;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0, -0.1f);
-        transform.Translate(0, 0.01f, 0);
+        transform.Rotate(0, 0, turnSpeed);
+        transform.Translate(0, moveSpeed, 0);
     }
 }
