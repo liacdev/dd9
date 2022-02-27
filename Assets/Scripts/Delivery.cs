7a0f75716dbf3db3cbd967df35500ab6c64f8f08
SouthpawCurveConcepts <jagoblacksky@gmail.com>
2022-02-26 19:05:52 -0600

    hasPackage

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    bool hasPackage;
     void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("Ouch!");
     }
 
      void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Package"){
-        Debug.Log("Got package!");
+            if(!hasPackage){
+                hasPackage = true;    
+                Debug.Log("Got package! HasPackage: " + hasPackage);
+            }else{
+                Debug.Log("You already have the package. HasPackage: " + hasPackage);
+            }
+
         }
 
         if (other.tag == "Customer"){
-        Debug.Log("Package delivered!");
+            if(hasPackage){
+                hasPackage = false;
+                Debug.Log("Package delivered! HasPackage: " + hasPackage);
+            }else{
+                Debug.Log("No package to deliver. HasPackage: " + hasPackage);
+            }
         }
     }
 }
