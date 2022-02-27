df9d5535760c52daab3ddcb23ebf84d17d5a4682
SouthpawCurveConcepts <jagoblacksky@gmail.com>
2022-02-26 21:52:10 -0600

    GetComponent

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -5,7 +5,15 @@ using UnityEngine;
 public class Delivery : MonoBehaviour
 {
     [SerializeField] bool hasPackage;
+    [SerializeField] Color32 hasPackageColor = new Color32(1, 1, 1, 1);
+    [SerializeField] Color32 noPackageColor = new Color32(1, 1, 1, 1);
     [SerializeField] float destroyDelay = 0.5f;
+    SpriteRenderer spriteRenderer;
+
+    void Start(){
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
     void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("Ouch!");
     }
@@ -14,6 +22,7 @@ public class Delivery : MonoBehaviour
         if (other.tag == "Package"){
             if(!hasPackage){
                 hasPackage = true;    
+                spriteRenderer.color = hasPackageColor;
                 Destroy(other.gameObject, destroyDelay);
                 Debug.Log("Got package! HasPackage: " + hasPackage);
             }else{
@@ -25,6 +34,7 @@ public class Delivery : MonoBehaviour
         if (other.tag == "Customer"){
             if(hasPackage){
                 hasPackage = false;
+                spriteRenderer.color = noPackageColor;
                 Debug.Log("Package delivered! HasPackage: " + hasPackage);
             }else{
                 Debug.Log("No package to deliver. HasPackage: " + hasPackage);
